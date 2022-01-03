cff09720d23f84fa4ae46005857d7c5d7a1a4844
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-01-03 12:51:40 +0100

    fix: append on_new_config instead of replacing it

--- lua/lua-dev/sumneko.lua
+++ lua/lua-dev/sumneko.lua
@@ -63,16 +63,20 @@ function M.types()
 end
 
 function M.setup(opts)
+  local lspconfig = require("lspconfig")
   return {
-    on_new_config = function(config, root_dir)
-      -- remove the root_dir from the workspace, otherwise diagnostics break. See #21
-      -- Should no longer be needed with workspace.nvim
-      local lib = vim.tbl_deep_extend("force", {}, config.settings.Lua.workspace.library)
-      lib[vim.loop.fs_realpath(root_dir) .. "/lua"] = nil
-      lib[vim.loop.fs_realpath(root_dir)] = nil
-      config.settings.Lua.workspace.library = lib
-      return config
-    end,
+    on_new_config = lspconfig.util.add_hook_after(
+      lspconfig.util.default_config.on_new_config,
+      function(config, root_dir)
+        -- remove the root_dir from the workspace, otherwise diagnostics break. See #21
+        -- Should no longer be needed with workspace.nvim
+        local lib = vim.tbl_deep_extend("force", {}, config.settings.Lua.workspace.library)
+        lib[vim.loop.fs_realpath(root_dir) .. "/lua"] = nil
+        lib[vim.loop.fs_realpath(root_dir)] = nil
+        config.settings.Lua.workspace.library = lib
+        return config
+      end
+    ),
     settings = {
       Lua = {
         runtime = {
