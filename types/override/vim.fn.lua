43f51e9b86d1637644f0fbba5e1e11bef4341750
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-14 09:37:18 +0200

    feat: added overrides for vim.fn.glob

--- types/override/vim.fn.lua
+++ types/override/vim.fn.lua
@@ -11,6 +11,19 @@ return {
       { type = "string" },
     },
   },
+  ["vim.fn.glob"] = {
+    params = {
+      { name = "expr", type = "string" },
+      { name = "nosuf", type = "boolean", optional = true },
+    },
+    overload = {
+      "fun(expr:string, nosuf?:boolean, list:true):string[]",
+      "fun(expr:string, nosuf?:boolean, list:true, alllinks:true):string[]",
+    },
+    ["return"] = {
+      { type = "string" },
+    },
+  },
   ["vim.fn.sign_define"] = {
     overload = {
       "fun(name:string, dict?:table)",
