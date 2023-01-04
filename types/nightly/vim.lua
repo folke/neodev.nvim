8d2a950045450927d8f7652ef26548b28f3137c8
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-04 20:43:24 +0100

    feat: added vim.treesitter.language

--- types/nightly/vim.lua
+++ types/nightly/vim.lua
@@ -14,6 +14,7 @@ vim.lsp = require("vim.lsp")
 vim.treesitter = require("vim.treesitter")
 vim.treesitter.highlighter = require("vim.treesitter.highlighter")
 vim.treesitter.query = require("vim.treesitter.query")
+vim.treesitter.language = require("vim.treesitter.language")
 vim.ui = require("vim.ui")
 vim.uri_from_bufnr = require("vim.uri").uri_from_bufnr
 vim.uri_from_fname = require("vim.uri").uri_from_fname
