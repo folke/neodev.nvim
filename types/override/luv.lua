dd630312eb978f554006a020554d64c197887644
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-06 16:55:17 +0100

    fix: revert #117

--- types/override/luv.lua
+++ types/override/luv.lua
@@ -1,7 +1,3 @@
----@class vim.loop
-local luv = vim.loop
-vim.loop = luv
-
 ---@class vim.loop.Timer
 ---@field start fun(timer:vim.loop.Timer, timeout:integer, repeat:integer, callback:fun())
 ---@field stop fun(timer:vim.loop.Timer)
