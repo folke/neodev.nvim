76bdb81a3cd45ff0f42c7d427e3e67fe6e39593c
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-24 14:24:51 +0200

    feat: added vim.iter, vim.version and vim.secure. Fixes #153

--- types/nightly/vim.lua
+++ types/nightly/vim.lua
@@ -12,17 +12,20 @@ vim.fs = require("vim.fs")
 vim.health = require("vim.health")
 vim.highlight = require("vim.highlight")
 vim.inspect = require("vim.inspect").inspect
+vim.iter = require("vim.iter")
 vim.keymap = require("vim.keymap")
 vim.lsp = require("vim.lsp")
+vim.secure = require("vim.secure")
 vim.treesitter = require("vim.treesitter")
 vim.treesitter.highlighter = require("vim.treesitter.highlighter")
-vim.treesitter.query = require("vim.treesitter.query")
 vim.treesitter.language = require("vim.treesitter.language")
+vim.treesitter.query = require("vim.treesitter.query")
 vim.ui = require("vim.ui")
 vim.uri_from_bufnr = require("vim.uri").uri_from_bufnr
 vim.uri_from_fname = require("vim.uri").uri_from_fname
 vim.uri_to_bufnr = require("vim.uri").uri_to_bufnr
 vim.uri_to_fname = require("vim.uri").uri_to_fname
+vim.version = require("vim.version")
 
 ---@type table<string,any>
 vim.g = {}
